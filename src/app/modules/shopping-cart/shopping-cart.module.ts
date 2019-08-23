96c35bf32d178a4ab0b663c0240da9c6972959f2
Surendra Gullipalli <surendra.gullipalli6@gmail.com>
2019-08-23 12:39:23 +0530

    Added Shopping Cart Basic layout

--- src/app/modules/shopping-cart/shopping-cart.module.ts
+++ src/app/modules/shopping-cart/shopping-cart.module.ts
@@ -5,9 +5,15 @@ import { ShoppingCartRoutingModule } from './shopping-cart-routing.module';
 import {DemoMaterialModule} from '../../../material-module';    // Angular material components
 
 import { ShoppingCartLayoutComponent } from '../../components/shopping-cart/shopping-cart-layout/shopping-cart-layout.component';
+import { HeaderComponent } from '../../components/shopping-cart/header/header.component';
+import { SidenavComponent } from '../../components/shopping-cart/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [ShoppingCartLayoutComponent],
+  declarations: [
+    ShoppingCartLayoutComponent,
+    HeaderComponent,
+    SidenavComponent
+  ],
   imports: [
     CommonModule,
     ShoppingCartRoutingModule,
